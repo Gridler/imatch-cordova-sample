9db69a4f251c683df65459618120c52c93933b10
bpiservicesdev <it@bpiservices.eu>
2018-11-15 11:55:00 +0100

    Reference cordova-plugin-jj2000 from NPM

--- cordova-plugin-jj2000/README.md
+++
@@ -1,8 +0,0 @@
-# cordova-plugin-jj2000
-Cordova plugin for JJ2000.
-
-## Install
-Install plugin:
-```
-cordova plugin add cordova-plugin-jj2000
-```
--- cordova-plugin-jj2000/www/jj2000.js
+++
@@ -1,8 +0,0 @@
-/*global cordova*/
-module.exports = {
-
-    convertJJ2000: function (data, success, failure) {
-        cordova.exec(success, failure, "JJ2000", "convertJJ2000", [data]);
-    }
-
-}
